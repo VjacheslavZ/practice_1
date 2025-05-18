c0845d336dd2b51f0481122ebbf093acbb418c23
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2025-05-18 19:24:26 +0300

    Update TypeScript configuration to target ES2023, add configuration interface and implementation, and refactor Table class into TableBuilder and TableBuilderDirector for improved OOP structure and data handling.

---
+++ src/task_1/oop/TableBuilder.ts
@@ -0,0 +1,132 @@
+import { MOCK_DATA } from '../mockData';
+import { Table } from './index';
+import { config, IConfig } from '../config';
+
+class AbstractTableBuilder {
+  constructor() {
+    const proto = Object.getPrototypeOf(this);
+
+    if (proto.constructor === AbstractTableBuilder) {
+      throw new Error('Abstract class should not be instantiated');
+    }
+  }
+
+  createInstance() {
+    throw new Error('Method createInstance should be implemented');
+  }
+
+  getInstance() {
+    throw new Error('Method getInstance should be implemented');
+  }
+
+  findMaxDensity() {
+    throw new Error('Method findMaxDensity should be implemented');
+  }
+
+  addDensityInterest() {
+    throw new Error('Method addDensityInterest should be implemented');
+  }
+
+  sortByInterest() {
+    throw new Error('Method sortByInterest should be implemented');
+  }
+
+  addPadding() {
+    throw new Error('Method addPadding should be implemented');
+  }
+
+  print(data?: string[][]) {
+    throw new Error('Method print should be implemented');
+  }
+}
+
+export class TableBuilder extends AbstractTableBuilder {
+  instance: Table;
+
+  constructor(data: string, config: IConfig) {
+    super();
+
+    this.instance = new Table(data, config);
+  }
+
+  createInstance() {
+    this.instance = new Table(MOCK_DATA, config);
+  }
+
+  getInstance() {
+    return this.instance;
+  }
+
+  findMaxDensity() {
+    const { densityColumnIndex } = this.instance.config;
+    let maxDensity = 0;
+    for (const line of this.instance.data) {
+      const density = parseFloat(line[densityColumnIndex]);
+      if (density > maxDensity) {
+        maxDensity = density;
+      }
+    }
+    this.instance.maxDensity = maxDensity;
+  }
+
+  addDensityInterest() {
+    const { densityColumnIndex } = this.instance.config;
+
+    const withDensityInterest = this.instance.data.map(row => {
+      const density = parseFloat(row[densityColumnIndex]);
+      const interest = Math.round(
+        (density * 100) / this.instance.maxDensity,
+      ).toString();
+      return [...row, interest];
+    });
+
+    this.instance.data = withDensityInterest;
+  }
+
+  sortByInterest() {
+    const sorted = this.instance.data.toSorted(
+      (a, b) =>
+        Number(b[this.instance.config.interestColumnIndex]) -
+        Number(a[this.instance.config.interestColumnIndex]),
+    );
+    this.instance.data = sorted;
+  }
+
+  addPadding() {
+    const columns = this.instance.data[0].length;
+    const longestByColumn: number[] = [];
+
+    for (let col = 0; col < columns; col++) {
+      let longestValue = this.instance.data[0][col];
+
+      for (let row = 1; row < this.instance.data.length; row++) {
+        const currentValue = this.instance.data[row][col];
+        const isLonger = currentValue.length > longestValue.length;
+        if (isLonger) {
+          longestValue = currentValue;
+        }
+      }
+      longestByColumn.push(longestValue.length);
+    }
+
+    const withPadding = this.instance.data.map(row =>
+      row.map((cell, cellIndex) => {
+        const padType = this.instance.config.columnsPadsPositions[cellIndex];
+        const maxLength = longestByColumn[cellIndex];
+
+        if (padType === 'end') {
+          return cell.padEnd(maxLength);
+        }
+        return cell.padStart(maxLength);
+      }),
+    );
+
+    return withPadding;
+  }
+
+  print(data = this.instance.data) {
+    for (const row of data) {
+      console.log(row.join(' '));
+    }
+  }
+}
