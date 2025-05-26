027325ad2b641e48c53cdbd68f1c8352c774a616
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2025-05-26 19:30:00 +0300

    Refactor and add tests for PurchaseIterator and Basket

---
+++ src/task_2/oop/index.test.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+const { describe, test } = require('node:test');
+const asset = require('node:assert');
+
+const { Basket } = require('./index.ts');
--- src/task_2/oop/index.ts
+++ src/task_2/oop/index.ts
@@ -1,170 +1,92 @@
+// const {} = require('Promise')
 import { MOCK_PURCHASE } from '../mockData';
 
 interface IPurchase {
   name: string;
   price: number;
 }
+// TODO split into files
+interface IThen {
+  addedItems: IPurchase[];
+  failedItems: IPurchase[];
+  total: number;
+}
+type TThen = (res: IThen) => IThen;
 
-// Variant 1
-{
-  class PurchaseIterator {
-    #purchase: IPurchase[] = [];
+class PurchaseIterator {
+  #purchase: IPurchase[] = [];
 
-    constructor(purchase: IPurchase[]) {
-      this.#purchase = purchase;
-    }
-
-    static create(purchase: IPurchase[]) {
-      return new PurchaseIterator(purchase);
-    }
-
-    [Symbol.asyncIterator]() {
-      let index = 0;
-      const self = this;
-      return {
-        async next(): Promise<IteratorResult<IPurchase>> {
-          const done = index >= self.#purchase.length;
-          const value = self.#purchase[index];
-          index++;
-
-          return { done, value };
-        },
-      };
-    }
+  constructor(purchase: IPurchase[]) {
+    this.#purchase = purchase;
   }
 
-  class Basket {
-    #limit: number;
-    #addedItems: IPurchase[] = [];
-    #failedItems: IPurchase[] = [];
-
-    constructor(constrains: { limit: number }) {
-      this.#limit = constrains.limit;
-    }
-
-    add(item: IPurchase) {
-      const isEnoughMoney = this.#limit >= item.price;
-      const isFirstItem = this.#addedItems.length === 0;
-
-      // add first item if it's not enough money
-      if ((!isEnoughMoney && isFirstItem) || isEnoughMoney) {
-        this.#addedItems.push(item);
-        this.#limit -= item.price;
-      }
-
-      if (!isEnoughMoney) {
-        this.#failedItems.push(item);
-      }
-    }
-
-    then(
-      resolve: (res: {
-        addedItems: IPurchase[];
-        failedItems: IPurchase[];
-        total: number;
-      }) => void,
-    ) {
-      return resolve({
-        addedItems: this.#addedItems,
-        failedItems: this.#failedItems,
-        total: this.#addedItems.reduce((acc, item) => acc + item.price, 0),
-      });
-    }
-
-    catch(err: any) {
-      console.log('catch', err);
-    }
+  static create(purchase: IPurchase[]) {
+    return new PurchaseIterator(purchase);
   }
 
-  const main = async () => {
-    const goods = PurchaseIterator.create(MOCK_PURCHASE);
-    const basket = new Basket({ limit: 1530 });
+  [Symbol.asyncIterator]() {
+    let index = 0;
+    const self = this;
+    return {
+      async next(): Promise<IteratorResult<IPurchase>> {
+        const done = index >= self.#purchase.length;
+        const value = self.#purchase[index];
+        index++;
 
-    for await (const item of goods) {
-      basket.add(item);
-    }
-
-    const result = basket.then(res => res);
-    console.dir(result, { depth: null });
-  };
-
-  main();
+        return { done, value };
+      },
+    };
+  }
 }
 
-// variant 2
-(async () => {
-  // Disposable can be used
-  interface IPurchase {
-    name: string;
-    price: number;
+class Basket {
+  #limit: number;
+  #addedItems: IPurchase[] = [];
+  #failedItems: IPurchase[] = [];
+
+  constructor(constrains: { limit: number }) {
+    this.#limit = constrains.limit;
   }
 
-  class PurchaseIterator {
-    #purchase: IPurchase[] = [];
-    onEnd: null | (() => void);
+  add(item: IPurchase) {
+    const isEnoughMoney = this.#limit >= item.price;
+    const isFirstItem = this.#addedItems.length === 0;
 
-    constructor(purchase: IPurchase[], onEnd: () => void) {
-      this.#purchase = purchase;
-      this.onEnd = onEnd;
+    // add first item if it's not enough money
+    if ((!isEnoughMoney && isFirstItem) || isEnoughMoney) {
+      this.#addedItems.push(item);
+      this.#limit -= item.price;
     }
 
-    static create(purchase: IPurchase[], onEnd: () => void) {
-      return new PurchaseIterator(purchase, onEnd);
-    }
-
-    #onEnd(callback: () => void) {
-      this.onEnd = callback;
-    }
-
-    [Symbol.asyncIterator]() {
-      let index = 0;
-      const self = this;
-      return {
-        async next(): Promise<IteratorResult<IPurchase>> {
-          const done = index >= self.#purchase.length;
-          const value = self.#purchase[index];
-          index++;
-          if (done && self.onEnd) {
-            self.onEnd();
-          }
-          return { done, value };
-        },
-      };
+    if (!isEnoughMoney) {
+      this.#failedItems.push(item);
     }
   }
 
-  class Basket {
-    #limit: number;
-    #items: IPurchase[] = [];
+  then(resolve: TThen) {
+    return resolve({
+      addedItems: this.#addedItems,
+      failedItems: this.#failedItems,
+      total: this.#addedItems.reduce((acc, item) => acc + item.price, 0),
+    });
+  }
+}
 
-    get items() {
-      return this.#items;
-    }
+const main = async () => {
+  const goods = PurchaseIterator.create(MOCK_PURCHASE);
+  const basket = new Basket({ limit: 1530 });
 
-    constructor(constrains: { limit: number }) {
-      this.#limit = constrains.limit;
-    }
-
-    add(item: IPurchase) {
-      this.#items.push(item);
-    }
-
-    then() {
-      console.log('then res', {
-        items: this.items,
-        total: this.#items.reduce((acc, item) => acc + item.price, 0),
-      });
-    }
+  for await (const item of goods) {
+    basket.add(item);
   }
 
-  const main = async () => {
-    const basket = new Basket({ limit: 1050 });
-    const goods = PurchaseIterator.create(MOCK_PURCHASE, () => basket.then());
+  const result = basket.then(res => res);
+  console.dir(result, { depth: null });
+};
 
-    for await (const item of goods) {
-      basket.add(item);
-    }
-    basket.then();
-  };
-  // main();
-})();
+main();
+
+module.exports = {
+  Basket,
+  PurchaseIterator,
+};
