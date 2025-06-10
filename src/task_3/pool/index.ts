4e0d50006c12d186843d8255cbce21e61c58288b
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2025-06-10 22:03:42 +0300

    Add class-based pool implementation in index.ts
    - Introduced a new `Pool` class to manage instances with a maximum capacity, enhancing the existing pool functionality.
    - Implemented a closure-based version alongside the class version for flexibility in usage.
    - Updated instance management methods to ensure proper handling of instances within the pool.

--- src/task_3/pool/index.ts
+++ src/task_3/pool/index.ts
@@ -1,5 +1,6 @@
 'use strict';
 
+// Closure version
 {
   const poolify = <T, O>(
     factory: (...args: O[]) => T,
@@ -28,6 +29,48 @@
   // Usage
   const createBufferFactory = (size: number) => new Uint8Array(size);
   const pool = poolify(createBufferFactory, [4096], 10, 15);
+
+  const instance = pool.getInstance();
+  pool.putInstance(instance);
+}
+
+// Class version
+{
+  class Pool<T, O> {
+    #factory: (...args: O[]) => T;
+    #options: O[];
+    #instances: T[] = [];
+    #max: number;
+
+    constructor(
+      factory: (...args: O[]) => T,
+      options: O[],
+      size: number,
+      max: number,
+    ) {
+      this.#factory = factory;
+      this.#options = options;
+      this.#max = max;
+
+      this.#instances = new Array(size)
+        .fill(null)
+        .map(() => factory(...options));
+    }
+
+    getInstance() {
+      const instance = this.#instances.pop() || this.#factory(...this.#options);
+      return instance;
+    }
+
+    putInstance(instance: T) {
+      if (this.#instances.length < this.#max) {
+        this.#instances.push(instance);
+      }
+    }
+  }
+  // Usage
+  const createBufferFactory = (size: number) => new Uint8Array(size);
+  const pool = new Pool(createBufferFactory, [4096], 10, 15);
 
   const instance = pool.getInstance();
   pool.putInstance(instance);
