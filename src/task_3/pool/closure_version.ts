394e9c7abb2ff60f48b25440759a71de7c7e0caa
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2025-06-11 16:54:06 +0300

    Enhance async instance management in Pool class
    - Improved the `getInstance` method to ensure proper handling of instance retrieval with asynchronous behavior.
    - Updated the `putInstance` method to throw an error when the pool is full and to manage the queue for waiting promises.
    - Refined the usage example to demonstrate the new asynchronous instance handling, including delayed returns of instances.

--- src/task_3/pool/closure_version.ts
+++ src/task_3/pool/closure_version.ts
@@ -9,13 +9,48 @@
   ) => {
     const instances = new Array(size).fill(null).map(() => factory(...options));
 
-    const getInstance = () => {
-      const instance = instances.pop() || factory(...options);
+    const queue: TResolve<T>[] = [];
+    const free = new Array(size).fill(true);
+    let current = 0;
+    let available = size;
+
+    const next = async () => {
+      if (!available) {
+        return new Promise<T>((resolve: TResolve<T>) => {
+          queue.push(resolve);
+        });
+      }
+      let instance = null;
+      let isFree = false;
+      do {
+        instance = instances[current];
+        isFree = free[current];
+        current++;
+      } while (!isFree || !instance);
+      return instance;
+    };
+
+    const getInstance = async () => {
+      const instance = await next();
+      if (!instance) return null;
+      const currentIndex = instances.indexOf(instance as T);
+      free[currentIndex] = false;
+      available--;
       return instance;
     };
     const putInstance = (instance: T) => {
-      if (instances.length < max) {
-        instances.push(instance);
+      if (instances.length >= max) {
+        throw new Error('Pool is full');
+      }
+      const currentIndex = instances.indexOf(instance);
+      free[currentIndex] = true;
+      available++;
+
+      if (instances.length > 0) {
+        const next = queue.shift();
+        if (next) {
+          setTimeout(next, 0, instance);
+        }
       }
     };
 
@@ -25,10 +60,32 @@
     };
   };
 
-  // Usage
-  const createBufferFactory = (size: number) => new Uint8Array(size);
-  const pool = poolify(createBufferFactory, [4096], 10, 15);
+  const main = async () => {
+    // Usage
+    const instancesToReturn: Uint8Array<ArrayBuffer>[] = [];
+    const createBufferFactory = (size: number) => new Uint8Array(size);
+    const pool = poolify(createBufferFactory, [2], 10, 15);
 
-  const instance = pool.getInstance();
-  pool.putInstance(instance);
+    setTimeout(() => {
+      pool.putInstance(instancesToReturn[0]);
+    }, 2000);
+
+    setTimeout(() => {
+      pool.putInstance(instancesToReturn[1]);
+    }, 3000);
+
+    setTimeout(() => {
+      pool.putInstance(instancesToReturn[2]);
+    }, 4000);
+
+    for (let i = 0; i < 13; i++) {
+      const instance = await pool.getInstance();
+      console.log(`instance #${i}`, instance);
+      if (i < 3 && instance) {
+        instancesToReturn.push(instance);
+      }
+    }
+  };
+
+  main();
 }
